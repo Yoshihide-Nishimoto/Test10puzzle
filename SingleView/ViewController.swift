227be5590701f67310ec4cd226c46bca9b8b2f00
西本至秀 <yoshi@nishimoto-itarishuu-no-mac.local>
2016-09-04 18:32:54 +0900

    Initial Commit

---
+++ SingleView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SingleView
+//
+//  Created by 西本 至秀 on 2016/09/04.
+//  Copyright © 2016年 西本 至秀. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
